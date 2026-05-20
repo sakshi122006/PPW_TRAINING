eb3197781dc93a583852163abae43ed79a067961
Sakshi Revaje <revajesakshi@gmail.com>
2026-05-20 15:47:01 +0530

    Add files via upload

--- Eqauls_string.java
+++ Eqauls_string.java
@@ -1,4 +1,29 @@
+import java.util.Scanner;
 public class Eqauls_string
 {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two strings:");
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+        int count = 0;
+        for(int i=0; i<str1.length(); i++)
+        {
+            if(str1.charAt(i) != str2.charAt(i))
+            {
+                count++;
+                break;
+            }
+        }
+        if(count == 0)
+        {
+            System.out.println("The two strings are equal.");
+        }
+        else
+        {
+            System.out.println("The two strings are not equal.");
+        }
+    }
     
 }

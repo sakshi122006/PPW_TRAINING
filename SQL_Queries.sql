5c4501880925dbe6cc4d5f02975c218247642511
Sakshi Revaje <revajesakshi@gmail.com>
2026-06-09 23:11:18 +0530

    Add left join query for counting non-transaction visits

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -2,3 +2,12 @@
 SELECT Product.product_name, Sales.year, Sales.price
 FROM Product, Sales
 WHERE Product.product_id = Sales.product_id;
+
+--left join
+SELECT v.customer_id,
+       COUNT(*) AS count_no_trans
+FROM Visits v
+LEFT JOIN Transactions t
+ON v.visit_id = t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY v.customer_id;

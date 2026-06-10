ee57045611a807147b2a795f6411a3d072dd36d6
Sakshi Revaje <revajesakshi@gmail.com>
2026-06-10 23:05:48 +0530

    Update SQL_Queries.sql

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -56,3 +56,15 @@ SELECT contest_id,
 FROM Register
 GROUP BY contest_id
 ORDER BY percentage DESC, contest_id ASC;
+
+-- round
+SELECT p.product_id,
+       ROUND(
+           IFNULL(SUM(u.units * p.price) / SUM(u.units), 0),
+           2
+       ) AS average_price
+FROM Prices p
+LEFT JOIN UnitsSold u
+ON p.product_id = u.product_id
+AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id;

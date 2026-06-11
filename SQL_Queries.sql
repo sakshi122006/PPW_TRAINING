c29a0eefdfa555057c2ac7f5e74a6270cff5c5df
Sakshi Revaje <revajesakshi@gmail.com>
2026-06-11 22:54:42 +0530

    Update SQL_Queries.sql

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -68,3 +68,9 @@ LEFT JOIN UnitsSold u
 ON p.product_id = u.product_id
 AND u.purchase_date BETWEEN p.start_date AND p.end_date
 GROUP BY p.product_id;
+
+-- AND
+SELECT product_id
+FROM Products
+WHERE low_fats = 'Y'
+  AND recyclable = 'Y';

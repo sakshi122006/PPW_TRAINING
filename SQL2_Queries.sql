bb90cb11d1c43e51426f72b45fa917a29a96112f
Sakshi Revaje <revajesakshi@gmail.com>
2026-06-11 23:23:32 +0530

    Update SQL2_Queries.sql

--- SQL2_Queries.sql
+++ SQL2_Queries.sql
@@ -9,3 +9,9 @@ SELECT name, population, area
 FROM World
 WHERE area >= 3000000
    OR population >= 25000000;
+
+-- null
+SELECT name
+FROM Customer
+WHERE referee_id != 2
+   OR referee_id IS NULL;

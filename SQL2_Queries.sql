bf14af25ef5c2d7aa259ca1e5b7ede0e3b928d29
Sakshi Revaje <revajesakshi@gmail.com>
2026-06-12 22:35:39 +0530

    Add average rating query for Queries table

--- SQL2_Queries.sql
+++ SQL2_Queries.sql
@@ -26,3 +26,11 @@ ORDER BY id;
 SELECT tweet_id
 FROM Tweets
 WHERE LENGTH(content) > 15;
+
+-- avg
+SELECT query_name,
+       ROUND(AVG(rating * 1.0 / position), 2) AS quality,
+       ROUND(AVG(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100, 2)
+       AS poor_query_percentage
+FROM Queries
+GROUP BY query_name;

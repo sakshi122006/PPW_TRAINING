f52e7d0fde0bf754d1473168418e3c9fbe42753a
Sakshi Revaje <revajesakshi@gmail.com>
2026-06-12 22:57:20 +0530

    Add query to delete duplicate Person entries
    Add a query to delete duplicate persons based on email.

--- SQL2_Queries.sql
+++ SQL2_Queries.sql
@@ -40,3 +40,9 @@ SELECT teacher_id,
        COUNT(DISTINCT subject_id) AS cnt
 FROM Teacher
 GROUP BY teacher_id;
+
+-- delete
+DELETE p1
+FROM Person p1, Person p2
+WHERE p1.email = p2.email
+AND p1.id > p2.id;

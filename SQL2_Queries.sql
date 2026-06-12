7f00c1cf58fe606df3f02522bc03ecc623ea272d
Sakshi Revaje <revajesakshi@gmail.com>
2026-06-12 23:01:22 +0530

    Add query to format user names
    Add a query to format user names with proper casing.

--- SQL2_Queries.sql
+++ SQL2_Queries.sql
@@ -46,3 +46,12 @@ DELETE p1
 FROM Person p1, Person p2
 WHERE p1.email = p2.email
 AND p1.id > p2.id;
+
+-- substring
+SELECT user_id,
+       CONCAT(
+           UPPER(LEFT(name, 1)),
+           LOWER(SUBSTRING(name, 2))
+       ) AS name
+FROM Users
+ORDER BY user_id;

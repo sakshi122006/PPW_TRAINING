59f01ec146779b5dbd86d009725e0f4d7f437925
Sakshi Revaje <revajesakshi@gmail.com>
2026-06-13 23:16:18 +0530

    Update SQL2_Queries.sql

--- SQL2_Queries.sql
+++ SQL2_Queries.sql
@@ -105,3 +105,13 @@ FROM Cinema
 WHERE id % 2 != 0
   AND description != 'boring'
 ORDER BY rating DESC;
+
+-- sum
+SELECT DATE_FORMAT(trans_date, '%Y-%m') AS month,
+       country,
+       COUNT(*) AS trans_count,
+       SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
+       SUM(amount) AS trans_total_amount,
+       SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+FROM Transactions
+GROUP BY DATE_FORMAT(trans_date, '%Y-%m'), country;

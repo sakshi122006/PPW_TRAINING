f39014ec10aca053d08617a4d2aff3953ad7c815
Sakshi Revaje <revajesakshi@gmail.com>
2026-06-13 23:00:53 +0530

    Add query to count active users between dates

--- SQL2_Queries.sql
+++ SQL2_Queries.sql
@@ -91,3 +91,10 @@ JOIN (
 ) t
 ON s.product_id = t.product_id
 AND s.year = t.first_year;
+
+--betwwen
+SELECT activity_date AS day,
+       COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
+GROUP BY activity_date;

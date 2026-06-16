e600af6900745b76b51f669dc646cba4ea4f1393
Sakshi Revaje <revajesakshi@gmail.com>
2026-06-16 22:38:44 +0530

    Implement multiplication of two numbers as strings

---
+++ leetcode_43.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        int a = Integer.parseInt(num1);
+        int b = Integer.parseInt(num2);
+        int c = a*b;
+        String str = String.valueOf(c);
+        return str;
+    }
+}

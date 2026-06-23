d5b3b37ae649e729d88ba6b4a3529347a47af213
Sakshi Revaje <revajesakshi@gmail.com>
2026-06-23 21:53:48 +0530

    Update SQL_Queries.sql

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -80,3 +80,14 @@ SELECT player_id,
        MIN(event_date) AS first_login
 FROM Activity
 GROUP BY player_id;
+
+--not in
+SELECT name
+FROM SalesPerson
+WHERE sales_id NOT IN (
+    SELECT o.sales_id
+    FROM Orders o
+    JOIN Company c
+    ON o.com_id = c.com_id
+    WHERE c.name = 'RED'
+);

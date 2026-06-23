6fbdcf6447b5aa49a5ef7718b8e00664f1772d66
Sakshi Revaje <revajesakshi@gmail.com>
2026-06-23 21:55:48 +0530

    Update SQL_Queries.sql

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -91,3 +91,13 @@ WHERE sales_id NOT IN (
     ON o.com_id = c.com_id
     WHERE c.name = 'RED'
 );
+
+-- between
+SELECT p.product_name,
+       SUM(o.unit) AS unit
+FROM Products p
+JOIN Orders o
+ON p.product_id = o.product_id
+WHERE o.order_date BETWEEN '2020-02-01' AND '2020-02-29'
+GROUP BY p.product_id, p.product_name
+HAVING SUM(o.unit) >= 100;

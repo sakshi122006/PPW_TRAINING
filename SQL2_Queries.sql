7ff71e2abfcc04ab105dddefc0417485d5e0040e
Sakshi Revaje <revajesakshi@gmail.com>
2026-06-24 14:35:07 +0530

    Update SQL2_Queries.sql

--- SQL2_Queries.sql
+++ SQL2_Queries.sql
@@ -206,3 +206,16 @@ SELECT (
     ORDER BY salary DESC
     LIMIT 1 OFFSET 1
 ) AS SecondHighestSalary;
+
+-- function
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+    SET N = N - 1;
+
+    RETURN (
+        SELECT DISTINCT salary
+        FROM Employee
+        ORDER BY salary DESC
+        LIMIT 1 OFFSET N
+    );
+END

65477886fcda48a6f5e2936be9b1c43b3f5f4e86
Sakshi Revaje <revajesakshi@gmail.com>
2026-06-26 17:36:59 +0530

    Implement createCounter function
    Add createCounter function that increments a number.

--- javascript.js
+++ javascript.js
@@ -13,3 +13,23 @@ var createHelloWorld = function() {
  * const f = createHelloWorld();
  * f(); // "Hello World"
  */
+
+
+/*2620*/
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    
+    return function() {
+        return n++;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */

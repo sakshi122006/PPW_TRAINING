008de92497c3a1cd12b0674fdbf595a85d4d0886
Sakshi Revaje <revajesakshi@gmail.com>
2026-06-26 17:33:24 +0530

    Add createHelloWorld function that returns greeting

---
+++ javascript.js
@@ -0,0 +1,15 @@
+/*2667*/
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */

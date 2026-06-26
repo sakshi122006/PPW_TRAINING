7970bc1f80781da0fc5dff600d60f1305e74a1e4
Sakshi Revaje <revajesakshi@gmail.com>
2026-06-26 19:07:04 +0530

    Add expect function for value assertions

--- javascript.js
+++ javascript.js
@@ -57,3 +57,27 @@ var expect = function(val) {
         }
     };
 };
+
+
+/*2704*/
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: function(expected) {
+            if (val === expected) {
+                return true;
+            }
+            throw new Error("Not Equal");
+        },
+        
+        notToBe: function(expected) {
+            if (val !== expected) {
+                return true;
+            }
+            throw new Error("Equal");
+        }
+    };
+};

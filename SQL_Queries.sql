43d774abe096b04427c14439f7e96d0ecef6ed46
Sakshi Revaje <revajesakshi@gmail.com>
2026-06-26 17:26:31 +0530

    Update SQL_Queries.sql

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -122,3 +122,20 @@ WHERE (e.departmentId, e.salary) IN (
     FROM Employee
     GROUP BY departmentId
 );
+
+-- partition by
+SELECT
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary
+FROM (
+    SELECT *,
+           DENSE_RANK() OVER (
+               PARTITION BY departmentId
+               ORDER BY salary DESC
+           ) AS rnk
+    FROM Employee
+) e
+JOIN Department d
+ON e.departmentId = d.id
+WHERE rnk <= 3;

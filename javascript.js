7ae200c506f1a1d7b3074f62758309fc51070174
Sakshi Revaje <revajesakshi@gmail.com>
2026-06-28 17:50:38 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -90,3 +90,15 @@ var expect = function(val) {
 var isEmpty = function(obj) {
     return Object.keys(obj).length === 0;
 };
+
+
+/*2723*/
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    const [value1, value2] = await Promise.all([promise1, promise2]);
+    return value1 + value2;
+};

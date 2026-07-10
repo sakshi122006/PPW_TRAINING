c5cbaa3fd51fdf1fb07ecfb4aedde26adf4d5582
Sakshi Revaje <revajesakshi@gmail.com>
2026-07-10 12:03:45 +0530

    Implement findFinalValue method in Solution class

---
+++ leetcode _2154.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        boolean found = true;
+        while(found)
+        {
+            found = false;
+            for(int i=0; i<nums.length; i++)
+            {
+                if(nums[i]==original)
+                {
+                    original = 2 * original;
+                    found = true;
+                    break;
+                }
+            }
+        }
+        return original;
+    }
+}

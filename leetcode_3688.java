b90290aaeab49d33ef334da046658424a965b9ba
Sakshi Revaje <revajesakshi@gmail.com>
2026-07-20 10:50:51 +0530

    Implement method to compute OR of even numbers

---
+++ leetcode_3688.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int evenNumberBitwiseORs(int[] nums) {
+        int ans = 0;
+        for(int num: nums)
+        {
+            if(num%2==0)
+            {
+                ans |= num;
+            }
+        }
+        return ans;
+    }
+}

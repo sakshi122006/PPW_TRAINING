8caf61b781276d139bee367aad689b1af7bb5f7c
Sakshi Revaje <revajesakshi@gmail.com>
2026-07-22 17:46:13 +0530

    Implement vowel and consonant scoring method

---
+++ leetcode_3813.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int vowelConsonantScore(String s) {
+        int vowel = 0;
+        int consonent = 0;
+        for(int i=0; i<s.length(); i++)
+        {
+            char ch = s.charAt(i);
+            if(ch>='a' && ch<='z')
+            {
+                if(ch!='a' && ch!='e' && ch!='i' &&  ch!='o' && ch!='u')
+                {
+                    consonent ++;
+                }
+                else
+                {
+                    vowel++;
+                }
+            }
+        }
+        if(consonent==0)
+        {
+            return 0;
+        }
+        int score = vowel/consonent;
+        return score;
+    }
+}

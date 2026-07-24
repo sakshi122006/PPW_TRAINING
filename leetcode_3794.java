9b80f89411d42f83dfa4fc905583cade284d8daa
Sakshi Revaje <revajesakshi@gmail.com>
2026-07-24 11:54:23 +0530

    Implement reversePrefix method in Solution class

---
+++ leetcode_3794.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String reversePrefix(String s, int k) {
+        char[] ch = s.toCharArray();
+        int left = 0;
+        int right = k-1;
+        while(left<right)
+        {
+            char temp = ch[left];
+            ch[left] = ch[right];
+            ch[right] = temp;
+
+            left++;
+            right--;
+        }
+        return new String(ch);
+    }
+}

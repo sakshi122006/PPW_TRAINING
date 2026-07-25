62c3ae6c7da8a3969661ce47db8c848e9eb26e75
Sakshi Revaje <revajesakshi@gmail.com>
2026-07-25 12:13:18 +0530

    Implement smallestNumber method in Solution class

---
+++ leetcode_3345.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int smallestNumber(int n, int t) {
+        while(true)
+        {
+            int product = 1;
+            int temp = n;
+
+            while(temp>0)
+            {
+                int digit = temp % 10;
+                product = product * digit;
+                temp = temp / 10;
+            }
+
+            if(product % t == 0)
+            {
+                return n;
+            }
+            n++;
+        }
+    }
+}

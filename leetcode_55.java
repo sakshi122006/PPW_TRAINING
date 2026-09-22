183446abc8e6c1bdeb982dbbe118d87696af97a6
Sakshi Revaje <revajesakshi@gmail.com>
2026-09-22 19:11:11 +0530

    Create leetcode_55.java

---
+++ leetcode_55.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxI = 0;
+        for(int i=0;i<nums.length;i++){
+            if(i>maxI) return false;
+            maxI = Math.max(maxI,i+nums[i]);
+        }
+        return true;
+    }
+}
